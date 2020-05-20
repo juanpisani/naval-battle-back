8e40150d819240e7813623b0df2d66fbb1d77476
fabrids23 <fabrids23@gmail.com>
2020-05-20 04:23:43 -0300

    Players connecting but we are not receiving messages

--- back/routing.py
+++ back/routing.py
@@ -4,6 +4,6 @@ from django.urls import re_path
 from back import consumers
 
 websocket_urlpatterns = [
-    re_path(r'api/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    # re_path(r'api/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
     re_path(r'api/game/(?P<session_id>\w+)/$', consumers.GameSessionConsumer)
 ]
